f97e415010d30a5fb2d793e48e225013ef441c5b
bdey05 <73259986+bdey05@users.noreply.github.com>
2021-10-10 00:47:26 -0400

    Add files via upload

--- app/src/main/java/com/example/coldcalling/Icons.java
+++ app/src/main/java/com/example/coldcalling/Icons.java
@@ -1,22 +1,60 @@
 package com.example.coldcalling;
 
+import java.util.Locale;
+
 public class Icons {
 
     private int mImageResId;
 
     private String mName;
 
-    public Icons(int imageResId, String name) {
+    private int mCalled;
+
+    private String mTime;
+
+    public Icons(int imageResId, String name, int called , String time) {
         mImageResId = imageResId;
-        mName = name;
+        name.replaceAll(" ", "");
+        String[]names = name.split("_");
+        String fName = "";
+        String lName = "";
+        if(names.length == 2) {
+            fName = names[0].substring(0,1).toUpperCase() + names[0].substring(1);
+            lName = names[1].substring(0,1).toUpperCase() + names[1].substring(1);
+        }
+
+        mName = fName + " " + lName;
+
+        mCalled = called;
+
+        mTime = time;
+
     }
 
     public int getImageResId() { return mImageResId; }
 
     public void setImageResId(int imageResId) { mImageResId = imageResId; }
 
-    public String getName() { return mName; }
+    public String getName() {
+        return mName;
+    }
 
     public void setName(String name) { mName = name; }
+
+    public void setCalled(int called) {
+        mCalled = called;
+    }
+
+    public int getCalled(){
+        return mCalled;
+    }
+
+    public String getTime(){
+        return mTime;
+    }
+    public void setTime(String time){
+        mTime = time;
+    }
+
 }
 
--- app/src/main/java/com/example/coldcalling/MainActivity.java
+++ app/src/main/java/com/example/coldcalling/MainActivity.java
@@ -1,38 +1,65 @@
 package com.example.coldcalling;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView dateAndTime, name;
+    private TextView dateAndTime, name, className;
     private Button calledLog, uncalledLog, random;
     private ImageView image;
+    private ArrayList<Icons> Icons= new ArrayList<Icons>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dateAndTime = (TextView) findViewById(R.id.dateAndTime);
+        className = (TextView) findViewById(R.id.className);
+        className.setText("App Development");
+
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 String date = new SimpleDateFormat("MM/dd/yyy").format(new Date());
-                String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
-                dateAndTime.setText("Date: " + date + " Time: " + time);
+                String time = new SimpleDateFormat("hh:mm a").format(new Date());
+                dateAndTime.setText("Date: " + date +" Time: " + time);
             }
         }, 0, 1000);
-
+        Field[] fields = R.drawable.class.getFields();
+        for (Field field : fields) {
+            if (!field.getName().startsWith("ic") && !field.getName().startsWith("abc")
+                    && !field.getName().startsWith("avd") && !field.getName().startsWith("tool")
+                    && !field.getName().startsWith("btn") && !field.getName().startsWith("design")
+                    && !field.getName().startsWith("material") && !field.getName().startsWith("mtrl")
+                    && !field.getName().startsWith("notif") && !field.getName().startsWith("test")
+                    && !field.getName().startsWith("navi")){
+                try {
+                    Icons.add(new Icons(field.getInt(null), field.getName(), 0, new SimpleDateFormat("hh:mm a").format(new Date())));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         addListenOnButton();
     }
 
@@ -40,8 +67,27 @@ public class MainActivity extends AppCompatActivity {
         name = (TextView) findViewById(R.id.name);
         calledLog = (Button) findViewById(R.id.calledLog);
         uncalledLog = (Button) findViewById(R.id.uncalledLog);
+        image = (ImageView) findViewById(R.id.image);
         random = (Button) findViewById(R.id.Random);
-        image = (ImageView) findViewById(R.id.image); 
+        random.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Random rand= new Random();
+                int index = rand.nextInt(Icons.size());
+                if(Icons.get(index).getCalled() != 2){
+                    Icons x = Icons.get(index);
+                    image.setImageResource(x.getImageResId());
+                    name.setText(x.getName());
+                    if(Icons.get(index).getCalled() < 2) {
+                        x.setCalled(x.getCalled() + 1);
+                    }
+                }
+                else{
+                    String message = Icons.get(index).getName() + " has already been called twice in the past 40 minutes. Click randomize again to call another person.";
+                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                }
 
+            }
+        });
     }
 }
